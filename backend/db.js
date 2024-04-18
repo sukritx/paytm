dde2eb99965a814ac04cc1491d8fedba4c132339
sukritx <ddserial@gmail.com>
2024-04-18 13:12:04 +0700

    add .env

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+require ('dotenv').config();
+
+mongoose.connect('process.env.MONGO_URI')
 
 const userSchema = new Schema({
     firstName: String,
