6055c87b43777b52a29fe9a00171b7fd7a82395d
sukritx <ddserial@gmail.com>
2024-04-18 16:22:27 +0700

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 
 const RootRouter = require('./routes/index.js')
 const UserRouter = require('./routes/user.js')
+
+app.use(cors())
+
+const app = express();
 
 /*
 routes
