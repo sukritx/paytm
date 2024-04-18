4dc37109f6e3f6878cd26787b1d203b1695a357b
sukritx <ddserial@gmail.com>
2024-04-18 16:16:59 +0700

    add user route

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 
 const RootRouter = require('./routes/index.js')
+const UserRouter = require('./routes/user.js')
 
 /*
 routes
 */
 app.use('/api/v1', require('RootRouter'));
+app.use('/api/v1/user', require('UserRouter'));
