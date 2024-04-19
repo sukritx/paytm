63b08c0bea757b2535694af4831387204045687c
sukritx <ddserial@gmail.com>
2024-04-20 02:35:38 +0700

    add transfer route

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { authMiddleware } = require('../middleware');
 const router = express.Router();
 const Account = require('../db.js')
@@ -12,5 +13,41 @@ router.get('/balance', authMiddleware, async (req,res) => {
         balance: account.balance
     })
 })
+
+router.post('/transfer', authMiddleware, async (req,res) => {
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+    const { amount, to } = req.body;
+
+    // Fetch the account
+    const account = await Account.findOne({ userId: req.userId }).session(session);
+
+    if(!account || account.balance < amount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Insufficient balance"
+        });
+    }
+
+    const toAccount = await Account.findOne({ userId: to }).session(session);
+
+    if (!toAccount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Invalid recipient"
+        });
+    }
+
+    // Process the transaction
+    await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
+    await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
+
+    // Commit the transaction
+    await session.commitTransaction();
+    res.json({
+        message: "Transfer successful"
+    });
+});
 
 module.exports = router;
