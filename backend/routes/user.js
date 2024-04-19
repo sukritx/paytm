5862f69f3375d5efc603e511d0a37a89097d8e32
sukritx <ddserial@gmail.com>
2024-04-20 00:50:31 +0700

    add get user by search route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -26,7 +26,7 @@ const updateBody = zod.object({
     lastName: zod.string().optional(),
 })
 
-router.post('/api/v1/user/signup', async (req, res) => {
+router.post('/signup', async (req, res) => {
     const { success } = signupBody.safeParse(req.body);
     if (!success) {
         return res.status(411).json({ error: 'Email already taken / Incorrect inputs' });
@@ -57,7 +57,7 @@ router.post('/api/v1/user/signup', async (req, res) => {
     })
 })
 
-router.post('/api/v1/user/signin', async (req, res) => {
+router.post('/signin', async (req, res) => {
     const { success } = signinBody.safeParse(req.body);
     if (!success) {
         return res.status(411).json({
@@ -65,7 +65,7 @@ router.post('/api/v1/user/signin', async (req, res) => {
         });
     }
 
-    const user = user.findOne({
+    const user = await user.findOne({
         username: req.body.username,
         password: req.body.password
     });
@@ -86,7 +86,7 @@ router.post('/api/v1/user/signin', async (req, res) => {
     })
 })
 
-router.put('/api/v1/user', authMiddleware, async (req, res) => {
+router.put('/user', authMiddleware, async (req, res) => {
     const { success } = updateBody.safeParse(req.body)
     if (!success) {
         res.status(411).json({
@@ -98,6 +98,31 @@ router.put('/api/v1/user', authMiddleware, async (req, res) => {
 
     res.json({
         message: "Updated successfully"
+    })
+})
+
+router.get('/bulk', authMiddleware, async (req,res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter,
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
     })
 })
 
